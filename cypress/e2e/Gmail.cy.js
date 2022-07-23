3599a03d317898e588e23442e8501393b8770dcd
phakjiramhsy <snsdlovely@outlook.co.th>
2022-07-23 14:15:16 +0700

    [ADD] go to website 2

--- cypress/e2e/Gmail.cy.js
+++ cypress/e2e/Gmail.cy.js
@@ -6,4 +6,12 @@ describe("Process for sign up email", () => {
          const sentArgs = { username: "username", password: "P@55w0rd!" };
       });
    });
+
+   it("Go to website", () => {
+      cy.origin("google.com", () => {
+         cy.visit("/intl/th/gmail/about/");
+         cy.get(".mobile-tablet-only").contains("ใช้งาน Gmail").click();
+         const sentArgs = { username: "username", password: "P@55w0rd!" };
+      });
+   });
 });
